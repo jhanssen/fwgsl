c2a3c1b3aa4b5d2df6c90e26173216b5c33a7d85
Jan Erik Hanssen <jhanssen@gmail.com>
2023-07-27 18:42:25 -0700

    twgsl -> fwgsl

--- Core/twgsl/tail.js
+++ Core/twgsl/tail.js
@@ -4,48 +4,48 @@ return Module;
     if (typeof exports === 'object' && typeof module === 'object')
       module.exports = factory();
     else if (typeof define === 'function' && define.amd)
-      define("twgsl", [], factory);
+      define("fwgsl", [], factory);
     else if (typeof exports === 'object')
-      exports["twgsl"] = factory();
-    else      root["twgsl"] = factory();
+      exports["fwgsl"] = factory();
+    else      root["fwgsl"] = factory();
   })(typeof self !== "undefined" ? self : typeof global !== "undefined" ? global : this, () => {
     const initialize = (wasmPath) => {
-      wasmPath = wasmPath || 'twgsl.wasm'
+      wasmPath = wasmPath || 'fwgsl.wasm'
       return new Promise(resolve => {
           Module({
               locateFile() {
                   return wasmPath;
               },
               onRuntimeInitialized() {
-                  var twgsl = this;
+                  var fwgsl = this;
                   var wgsl = "";
                   var textDecoder = new TextDecoder();
                   var convertSpirV2WGSL = (code) => {
-                    if (!twgsl._return_string_callback) {
-                        twgsl._return_string_callback = (data, length) => {
-                            const bytes = new Uint8ClampedArray(twgsl.HEAPU8.subarray(data, data + length));
+                    if (!fwgsl._return_string_callback) {
+                        fwgsl._return_string_callback = (data, length) => {
+                            const bytes = new Uint8ClampedArray(fwgsl.HEAPU8.subarray(data, data + length));
                             wgsl = textDecoder.decode(bytes);
                         };
                     }
-                    let addr = twgsl._malloc(code.byteLength);
-                    twgsl.HEAPU32.set(code, addr / 4);
-                    twgsl._spirv_to_wgsl(addr, code.byteLength);
-                    twgsl._free(addr);
+                    let addr = fwgsl._malloc(code.byteLength);
+                    fwgsl.HEAPU32.set(code, addr / 4);
+                    fwgsl._spirv_to_wgsl(addr, code.byteLength);
+                    fwgsl._free(addr);
                     return wgsl;
                   };
                   var entries = [];
                   var convertWGSL2SpirV = (code) => {
                       entries.splice(0, entries.length);
-                      if (!twgsl._return_entrypoint_callback) {
-                          twgsl._return_entrypoint_callback = (stage, data, length) => {
-                              const buffer = twgsl.HEAPU8.subarray(data, data + length);
+                      if (!fwgsl._return_entrypoint_callback) {
+                          fwgsl._return_entrypoint_callback = (stage, data, length) => {
+                              const buffer = fwgsl.HEAPU8.subarray(data, data + length);
                               entries.push(stage, buffer);
                           };
                       }
-                      let addr = twgsl._malloc(code.length);
-                      twgsl.HEAPU32.set(code, addr / 4);
-                      twgsl._wgsl_to_spirv(addr, code.length);
-                      twgsl._free(addr);
+                      let addr = fwgsl._malloc(code.length);
+                      fwgsl.HEAPU32.set(code, addr / 4);
+                      fwgsl._wgsl_to_spirv(addr, code.length);
+                      fwgsl._free(addr);
                       return entries;
                   };
                   resolve({
