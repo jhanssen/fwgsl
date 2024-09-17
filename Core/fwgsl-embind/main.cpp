a8f14d6d9b9158b7b0b06028938dad275321ff99
Jan Erik Hanssen <jhanssen@gmail.com>
2024-09-17 16:10:48 -0700

    convert color inputs to spirv input attachments

--- Core/fwgsl-embind/main.cpp
+++ Core/fwgsl-embind/main.cpp
@@ -2,6 +2,30 @@
 #include <emscripten/val.h>
 
 #include <tint/tint.h>
+#include <src/tint/lang/core/builtin_fn.h>
+#include <src/tint/lang/wgsl/reader/reader.h>
+#include <src/tint/lang/wgsl/writer/writer.h>
+#include <src/tint/lang/wgsl/program/clone_context.h>
+#include <src/tint/lang/wgsl/program/program_builder.h>
+#include <src/tint/lang/wgsl/program/program.h>
+#include <src/tint/lang/wgsl/ast/module.h>
+#include <src/tint/lang/wgsl/ast/enable.h>
+#include <src/tint/lang/wgsl/ast/function.h>
+#include <src/tint/lang/wgsl/ast/identifier.h>
+#include <src/tint/lang/wgsl/ast/identifier_expression.h>
+#include <src/tint/lang/wgsl/ast/type_decl.h>
+#include <src/tint/lang/wgsl/ast/transform/data.h>
+#include <src/tint/lang/wgsl/ast/transform/manager.h>
+#include <src/tint/lang/wgsl/ast/transform/transform.h>
+#include <src/tint/lang/wgsl/resolver/resolve.h>
+#include <src/tint/lang/wgsl/sem/array.h>
+#include <src/tint/lang/wgsl/sem/function.h>
+#include <src/tint/lang/wgsl/sem/variable.h>
+#include <src/tint/lang/spirv/reader/reader.h>
+#include <src/tint/lang/spirv/writer/writer.h>
+#include <src/tint/utils/diagnostic/source.h>
+#include <src/tint/utils/rtti/castable.h>
+#include <src/tint/utils/rtti/switch.h>
 
 #include <cstdlib>
 #include <array>
@@ -11,6 +35,125 @@
 #include <optional>
 
 using namespace emscripten;
+
+class FragColorInputToInputAttachment final : public tint::Castable<FragColorInputToInputAttachment, tint::ast::transform::Transform>
+{
+public:
+    FragColorInputToInputAttachment(bool* convertedColorInput)
+        : mConvertedColorInput(convertedColorInput)
+    {
+    }
+    virtual ~FragColorInputToInputAttachment() override = default;
+
+    virtual ApplyResult Apply(const tint::Program& src,
+                              const tint::ast::transform::DataMap& inputs,
+                              tint::ast::transform::DataMap& outputs) const override
+    {
+        tint::ProgramBuilder b;
+        tint::program::CloneContext ctx{&b, &src, /* auto_clone_symbols */ true};
+
+        bool inputEnabled = false;
+
+        for (auto* decl : src.AST().GlobalDeclarations()) {
+            Switch(
+                decl,
+                [&](const tint::ast::TypeDecl* ty) {
+                    b.AST().AddTypeDecl(ctx.Clone(ty));
+                },
+                [&](const tint::ast::Override* override) {
+                    b.AST().AddGlobalVariable(ctx.Clone(override));
+                },
+                [&](const tint::ast::Var* var) {
+                    b.AST().AddGlobalVariable(ctx.Clone(var));
+                },
+                [&](const tint::ast::Const* c) {
+                    b.AST().AddGlobalVariable(ctx.Clone(c));
+                },
+                [&](const tint::ast::Function* func) {
+                    for (const auto& param : func->params) {
+                        for (const auto& attr : param->attributes) {
+                            if (attr->Is<tint::ast::ColorAttribute>()) {
+                                const auto& paramName = param->name->symbol.Name();
+                                const auto& paramType = param->type.expr->identifier->symbol.Name();
+
+                                uint64_t idx = 0;
+
+                                const auto& colorAttr = attr->As<tint::ast::ColorAttribute>();
+                                if (!colorAttr->expr->Is<tint::ast::IntLiteralExpression>()) {
+                                    //fprintf(stderr, "expected int literal for ColorAttribute\n");
+                                    return;
+                                }
+                                idx = colorAttr->expr->As<tint::ast::IntLiteralExpression>()->value;
+
+                                // we need to rewrite this param
+                                //needsRewrite = true;
+                                ctx.Remove(func->params, param);
+
+                                if (!inputEnabled) {
+                                    inputEnabled = true;
+                                    *mConvertedColorInput = true;
+                                    b.AST().AddEnable(b.Enable(tint::wgsl::Extension::kChromiumInternalInputAttachments));
+                                }
+
+                                // add a global input attachment
+                                b.AST().AddGlobalVariable(b.Var(
+                                                              b.Ident("haltiInput_" + paramName),                        // name
+                                                              b.ty.input_attachment(b.ty.f32()),                          // type
+                                                              static_cast<const tint::ast::Expression*>(nullptr),                           // declared_address_space
+                                                              static_cast<const tint::ast::Expression*>(nullptr),                           // declared_access
+                                                              static_cast<const tint::ast::Expression*>(nullptr),                           // initializer
+                                                              tint::Vector<const tint::ast::Attribute*, 2>({
+                                                                      b.Group(tint::core::u32(idx)),
+                                                                      b.Binding(tint::core::u32(idx)),
+                                                                      b.InputAttachmentIndex(tint::core::u32(idx))
+                                                                  })
+                                                              ));
+                                // add a load variable to the function body
+                                ctx.InsertFront(func->body->statements, b.Decl(
+                                                    b.Var(
+                                                        b.Ident(paramName),
+                                                        b.ty.vec4(b.ty.f32()),
+                                                        static_cast<const tint::ast::Expression*>(nullptr),
+                                                        static_cast<const tint::ast::Expression*>(nullptr),
+                                                        b.Call(tint::core::BuiltinFn::kInputAttachmentLoad, b.Expr(b.Ident("haltiInput_" + paramName))),
+                                                        tint::Empty
+                                                        )));
+                            }
+                        }
+                    }
+
+                    b.AST().AddFunction(ctx.Clone(func));
+                },
+                [&](const tint::ast::Enable* ext) {
+                    for (auto* e : ext->extensions) {
+                        if (e->name == tint::wgsl::Extension::kChromiumExperimentalFramebufferFetch) {
+                            if (ext->extensions.Length() > 1) {
+                                ctx.Remove(ext->extensions, e);
+                            }
+                            return;
+                        } else if (e->name == tint::wgsl::Extension::kChromiumInternalInputAttachments) {
+                            return;
+                        }
+                    }
+                    b.AST().AddEnable(ctx.Clone(ext));
+                },
+                [&](const tint::ast::Requires*) {
+                    // Drop requires directives as they are optional, and it's non-trivial to determine
+                    // which features are needed for which entry points.
+                },
+                [&](const tint::ast::ConstAssert* ca) { b.AST().AddConstAssert(ctx.Clone(ca)); },
+                [&](const tint::ast::DiagnosticDirective* d) { b.AST().AddDiagnosticDirective(ctx.Clone(d)); },  //
+                TINT_ICE_ON_NO_MATCH);
+        }
+
+        return tint::resolver::Resolve(b);
+    }
+
+private:
+    bool* mConvertedColorInput;
+};
+
+TINT_INSTANTIATE_TYPEINFO(FragColorInputToInputAttachment);
 
 static inline const char* convertMessageType(tint::diag::Severity severity)
 {
@@ -59,10 +202,12 @@ public:
     bool hasError() const { return mError.has_value(); }
     std::string error() const { return mError.value(); }
     uint32_t numEntryPoints() const { return mEntryPoints.size(); }
+    bool convertedColorInputs() const { return mConvertedColorInputs; }
     EntryPoint entryPoint(uint32_t idx) const { return mEntryPoints[idx]; }
 
     std::optional<std::string> mError;
     std::vector<EntryPoint> mEntryPoints;
+    bool mConvertedColorInputs = false;
 };
 
 void Transpiler::wgslToSpirv(const std::string& filename, const std::string& wgsl)
@@ -74,6 +219,7 @@ void Transpiler::wgslToSpirv(const std::string& filename, const std::string& wgs
 
     mError = {};
     mEntryPoints.clear();
+    mConvertedColorInputs = false;
 
     if (!program.IsValid()) {
         std::string msg;
@@ -95,6 +241,8 @@ void Transpiler::wgslToSpirv(const std::string& filename, const std::string& wgs
 
         transform_manager.append(std::make_unique<tint::ast::transform::SingleEntryPoint>());
         transform_inputs.Add<tint::ast::transform::SingleEntryPoint::Config>(entryPoint.name);
+
+        transform_manager.append(std::make_unique<FragColorInputToInputAttachment>(&mConvertedColorInputs));
 
         auto outProgram = transform_manager.Run(program, std::move(transform_inputs), transform_outputs);
         if (!outProgram.IsValid()) {
@@ -151,6 +299,7 @@ EMSCRIPTEN_BINDINGS(fwgslwasm) {
     .function("wgslToSpirv", &Transpiler::wgslToSpirv)
     .function("hasError", &Transpiler::hasError)
     .function("error", &Transpiler::error)
+    .function("convertedColorInputs", &Transpiler::convertedColorInputs)
     .function("numEntryPoints", &Transpiler::numEntryPoints)
     .function("entryPoint", &Transpiler::entryPoint);
     class_<EntryPoint>("EntryPoint")
